702b94b359efa4b66d5b3038b94e80f8f155aadf
onarman <onarmannn@gmail.com>
2022-10-20 15:20:54 +0300

    mui project started

---
+++ src/component/navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import logoOn from "../../assets/logoOn.jpg"
+export default function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed" color="inherit" sx={{borderBottom:"1px solid rgba(0,0,0,0.12)"}}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <img src={logoOn} height="45px"></img>
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
